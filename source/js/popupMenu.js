d4ed9ab76dc660c5bba52fbdbec7abb2bc7e0d00
Boris Larchenko <borislarchenko@gmail.com>
2018-10-23 20:05:52 +0300

    make cross clickable in js file

--- source/js/popupMenu.js
+++ source/js/popupMenu.js
@@ -1,11 +1,14 @@
 var header = document.querySelector('.page-header');
 var menu = header.querySelector('.page-header__menu');
+var closeButton = header.querySelector('.main-nav__close');
 
 var myFunction = function (evt) {
   evt.preventDefault();
   var nav = header.querySelector('.main-nav');
   nav.classList.toggle("main-nav--closed");
   nav.classList.toggle("main-nav--opened");
+  menu.classList.toggle("page-header__menu--active");
 };
 
 menu.addEventListener("click", myFunction);
+closeButton.addEventListener("click", myFunction);
