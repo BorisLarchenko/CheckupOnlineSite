ff6fe93001dfc717c79e88be1339c18ef4e04666
Boris Larchenko <borislarchenko@gmail.com>
2018-10-31 13:48:22 +0200

    add js for modal window

---
+++ source/js/modal.js
@@ -0,0 +1,20 @@
+var callBackButton = document.querySelector('.call__callback');
+var backgroundOverlay = document.querySelector('.popup__overlay');
+var popupForm = document.querySelector('.popup__form');
+
+var closePopupForm = popupForm.querySelector('.popup__button');
+
+callBackButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  showHideModal();
+});
+
+closePopupForm.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  showHideModal();
+});
+
+var showHideModal = function () {
+  backgroundOverlay.classList.toggle('popup--inactive');
+  popupForm.classList.toggle('popup--inactive')
+};
